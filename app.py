d1dc89afe2efdcef901d6eb0764466db06276061
leno4kaaa <117352174+leno4kaaa@users.noreply.github.com>
2025-02-03 08:48:10 +0200

    4444

--- app.py
+++ app.py
@@ -34,7 +34,12 @@ def pievinot_rezultatu():
     dati = request.json
     try:
         pievienot(dati)
-        
+        top_5 = sorted(get_top_results(), key=lambda x: (x['klikski'], x['laiks']))[:5]
+        with open('result.json', 'w', encoding='utf-8') as f:
+            json.dump(top_5, f, ensure_ascii=False, indent=4)
+        return jsonify({'status': 'success'}), 200
+    except Exception:
+        return jsonify({'status': 'error'}), 500
     
 
 if __name__ == '__main__':
